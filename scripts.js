50a5ef349a6a6b8def776235036215f10343634b
Anilson <pitrbr@gmail.com>
2024-09-21 20:42:46 -0300

    capturando o evento de submit(enviar) do formulario.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
+//Obetendo os elementos do formulario.
+
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 
 // Manipulando o input amount para receber somente numero.
+
 amount.addEventListener("input", () =>{
     
     const hasCharactersRegex = /\D+/g
     amount.value = amount.value.replace(hasCharactersRegex,"")
 })
+
+// capturando o evento de submit(enviar) do formulario.
+
+form.onsubmit =(event) =>{
+    event.preventDefault()
+
+    console.log(currency.value)
+}
